f827f9a2946416ff4b34228ec868d6355a468ffb
jxstxn__ <Justinbaumann12112001@gmail.com>
2021-01-29 22:03:23 +0100

    Update github.js
    On PullRequest Event and PushRequest Event the Description send to discord

--- routes/github.js
+++ routes/github.js
@@ -6,7 +6,7 @@ hook.setUsername('GitHub Reporter'); // Can be replaced by any other funny name
 
 
 router.post('/', (req, res, next) => {
-    try{
+    try {
         const event = {
             action: req.headers["x-github-event"],
             name: req.body.repository.name,
@@ -16,49 +16,41 @@ router.post('/', (req, res, next) => {
             senderLink: req.body.sender["html_url"]
         }
         var embed;
-        if(event.action != null || event.action != undefined){
-            if(event.sender != null || event.sender != undefined){
-                if(event.senderImage != null || event.senderImage != undefined){
-                    if(event.senderLink != null || event.senderLink != undefined){
-                         embed = new MessageBuilder()
-                        .setTitle(event.action.toUpperCase() + ' at: ' + event.url)
-                        .setAuthor(event.sender, event.senderImage, event.senderLink)
-                        .setFooter('Hey! ' + event.sender + ' did action: "' + event.action + '" on your repository: ' + event.name + ' go check it out!');
-                    }else {
-                         embed = new MessageBuilder()
-                         .setTitle(event.action.toUpperCase() + ' at: ' + event.url)
-                         .setAuthor(event.sender, event.senderImage)
-                         .setFooter('Hey! ' + event.sender + ' did action: "' + event.action + '" on your repository: ' + event.name + ' go check it out at: ' + event.url);
-                    }
-                }else {
-                     embed = new MessageBuilder()
-                     .setTitle(event.action.toUpperCase() + ' at: ' + event.url)
-                     .setFooter('Hey! ' + event.sender + ' did action: "' + event.action + '" on your repository: ' + event.name + ' go check it out at: ' + event.url);
-                }
-            } else {
-                 embed = new MessageBuilder()
-                 .setTitle(event.action.toUpperCase() + ' at: ' + event.url)
-                 .setFooter('Hey! Something changed on your repository which I cant handle yet: ' + event.name + ' go check it out at: ' + event.url);
-            } 
-        } else {
+        if (event.action != null || event.action != undefined && event.sender != null || event.sender != undefined && vent.senderImage != null || event.senderImage != undefined && vent.senderLink != null || event.senderLink != undefined) {
             console.log(event.action);
-            console.log(event.name);
-            console.log(event.url);
-            console.log(event.sender);
-            console.log(event.senderImage);
-            console.log(event.senderLink);
+            if (event.action === "pull_request") {
+                embed = new MessageBuilder()
+                    .setTitle(event.action.toUpperCase() + ' at: ' + event.url)
+                    .setAuthor(event.sender, event.senderImage, event.senderLink)
+                    .setFooter('Hey! ' + event.sender + ' did action: "' + event.action + '" on your repository: ' + event.name + ' go check it out! Description: ' + req.body["pull_request"].title);
+            } else if (event.action === "push") {
+                var commitDescriptions = "";
+                req.body.commits.forEach(commit => {
+                    commitDescriptions = commitDescriptions + commit.message + ", ";
+                })
+                embed = new MessageBuilder()
+                    .setTitle(event.action.toUpperCase() + ' at: ' + event.url)
+                    .setAuthor(event.sender, event.senderImage, event.senderLink)
+                    .setFooter('Hey! ' + event.sender + ' did action: "' + event.action + '" on your repository: ' + event.name + ' go check it out! Description: ' + commitDescriptions);
+            } else {
+                embed = new MessageBuilder()
+                    .setTitle(event.action.toUpperCase() + ' at: ' + event.url)
+                    .setAuthor(event.sender, event.senderImage, event.senderLink)
+                    .setFooter('Hey! ' + event.sender + ' did action: "' + event.action + '" on your repository: ' + event.name + ' go check it out!');
+            }
+        } else {
             embed = new MessageBuilder()
-            .setTitle(event.url)
-            .setText('Hey! Someone did Something what we didnt handle at the moment');
-            
-        }
+                .setTitle(event.url)
+                .setText('Hey! Someone did Something what we didnt handle at the moment');
 
+        }
+        console.log(JSON.stringify(embed));
         hook.send(embed);
         res.status(200).json({
             message: 'Everything worked fine',
             messageSendToDiscord: JSON.stringify(embed)
         })
-    } catch (err){
+    } catch (err) {
         console.error(err);
         res.status(500).json({
             error: err
